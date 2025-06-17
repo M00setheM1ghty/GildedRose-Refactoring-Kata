e9048ffb062a02052fb9c92876a64847d2441f39
Baumann Thomas <tbaumann115@gmail.com>
2025-06-17 14:50:14 +0200

    added regular item beahviour tests for regular and edge cases(sellby and quality)

---
+++ csharp.NUnit/GildedRoseTests/ItemBehaviorTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using GildedRoseKata;
+
+namespace GildedRoseTests
+{
+    public class ItemBehaviorTests
+    {
+        [Test]
+        public void TestBeforeSellDate()
+        {
+            // Arrange
+            var items = new List<Item> { new Item { Name = "Normal Item", SellIn = 10, Quality = 20 } };
+            var app = new GildedRose(items);
+            // Act
+            app.UpdateQuality();
+            // Assert
+            Assert.That(items[0].SellIn, Is.EqualTo(9));
+            Assert.That(items[0].Quality, Is.EqualTo(19));
+        }
+
+        public void TestOnSellDate()
+        {
+            // Arrange
+            var items = new List<Item> { new Item { Name = "Normal Item", SellIn = 0, Quality = 20 } };
+            var app = new GildedRose(items);
+            // Act
+            app.UpdateQuality();
+            // Assert
+            Assert.That(items[0].SellIn, Is.EqualTo(-1));
+            Assert.That(items[0].Quality, Is.EqualTo(18));
+        }
+
+        public void TestAfterSellDate()
+        {
+            // Arrange
+            var items = new List<Item> { new Item { Name = "Normal Item", SellIn = -1, Quality = 20 } };
+            var app = new GildedRose(items);
+            // Act
+            app.UpdateQuality();
+            // Assert
+            Assert.That(items[0].SellIn, Is.EqualTo(-2));
+            Assert.That(items[0].Quality, Is.EqualTo(18));
+        }
+
+        public void TestQualityZero() {
+            var items = new List<Item> {new Item { Name = "Another Item", SellIn = -1, Quality = 0 } };
+            var app = new GildedRose(items);
+            // Act
+            app.UpdateQuality();
+            // Assert
+            Assert.That(items[0].SellIn, Is.EqualTo(-2));
+            Assert.That(items[1].Quality, Is.EqualTo(0));
+        }
+    }
+}
