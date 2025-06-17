7ce0596cd22cee5eae69be71d0a9f49288fa6336
Baumann Thomas <tbaumann115@gmail.com>
2025-06-17 15:09:41 +0200

    added sulfuras tests: no quality change for negative/positive/at 0 cases

---
+++ csharp.NUnit/GildedRoseTests/SulfurasTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using GildedRoseKata;
+
+namespace GildedRoseTests
+{
+    public class SulfurasTests
+    {
+        [Test]
+        public void TestSulfurasNoChangePositiveSellin()
+        {
+            // Arrange
+            var items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 10, Quality = 80 } };
+            var app = new GildedRose(items);
+            // Act
+            app.UpdateQuality();
+            // Assert
+            Assert.That(items[0].SellIn, Is.EqualTo(9));
+            Assert.That(items[0].Quality, Is.EqualTo(80));
+        }
+
+        public void TestSulfurasNoChangeNegativeSellin()
+        {
+            // Arrange
+            var items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = -5, Quality = 80 } };
+            var app = new GildedRose(items);
+            // Act
+            app.UpdateQuality();
+            // Assert
+            Assert.That(items[0].SellIn, Is.EqualTo(9));
+            Assert.That(items[0].Quality, Is.EqualTo(80));
+        }
+
+        public void TestSulfurasNoChangeSellinZero()
+        {
+            // Arrange
+            var items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 } };
+            var app = new GildedRose(items);
+            // Act
+            app.UpdateQuality();
+            // Assert
+            Assert.That(items[0].SellIn, Is.EqualTo(9));
+            Assert.That(items[0].Quality, Is.EqualTo(80));
+        }
+    }
+}
